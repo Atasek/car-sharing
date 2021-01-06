f9fad153089011a9768b29434f914e10087499c9
Atasek <55684330+atasek@users.noreply.github.com>
2021-01-06 18:10:48 +0400

    V-08: return cors proxy

--- src/api/common.js
+++ src/api/common.js
@@ -1,4 +1,4 @@
-export const CORS_URL = ''; // ВЕРНУТЬ!!!
+export const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
 export const BASE_URL =
     `${CORS_URL}http://api-factory.simbirsoft1.com/api/db/`;
 export const AUTH_URL =
